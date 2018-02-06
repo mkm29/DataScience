a5febced71c53e819ae1e0cd2290fec1a712e32a
Mitch Murphy <mitch.murphy@gmail.com>
2018-02-06 11:11:14 -0500

    Added function stubs for 2.3
    Former-commit-id: 5f34a5c23ec1e602f05cf5fe8708d18178664c5c

--- projects/ProgrammingAssignment3.R
+++ projects/ProgrammingAssignment3.R
@@ -20,3 +20,45 @@ outcome[, 11] <- as.numeric(outcome[, 11])
 hist(outcome[, 11])
 
 # initial setup complete, now write functions that operate on the outcome data
+
+best <- function(state, outcome) {
+  ## Read outcome data
+  
+  
+  ## Check that state and outcome are valid
+  
+  
+  ## Return hospital name in that state with lowest 30-day death
+}
+
+
+rankhospital <- function(state, outcome, num = "best") {
+  ## Read outcome data
+  
+  
+  ## Check that state and outcome are valid
+  
+  
+  ## Return hospital name in that state with the given rank
+  
+  
+  ## 30-day death rate
+}
+
+
+rankall <- function(outcome, num = "best") {
+  ## Read outcome data
+  
+  
+  ## Check that state and outcome are valid
+  
+  
+  ## For each state, find the hospital of the given rank
+  
+  
+  ## Return a data frame with the hospital names and the
+  
+  
+  ## (abbreviated) state name
+}
+
