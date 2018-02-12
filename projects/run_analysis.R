db96ea1856893c7cf24d6f4d8c9760d8d97e3488
Mitch Murphy <mitch.murphy@gmail.com>
2018-02-12 18:55:29 -0500

    Added the accelometer data used for the Clearning Data Course Project
    Former-commit-id: eaa0a3c454287a78ca6ea33e429adcc634487dd8
    Former-commit-id: c4ae9b338b32f886c33a4dfef023ff954b17c020

---
+++ projects/run_analysis.R
@@ -0,0 +1,44 @@
+##################################################
+## Project: Getting and Cleaning Data Course Project
+## Script: Read the accelometer data collected by UCI
+## from Samsung Galaxy phones, clean it, perform simple stats on it
+## Return a human-readable, formatted data frame 
+## Source: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+## Date: 2/12/18
+## Author: Mitch Murphy
+##################################################
+
+
+## Merges the training and the test sets to create one data set.
+
+
+
+
+
+
+
+## Extracts only the measurements on the mean and standard deviation for each measurement.
+
+
+
+
+
+
+
+## Uses descriptive activity names to name the activities in the data set
+
+
+
+
+
+
+
+## Appropriately labels the data set with descriptive variable names.
+
+
+
+
+
+
+
+## From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject
