6327ebed8a7ecca03644629dcaf4cb7a51730b2f
Mitch Murphy <mitch.murphy@gmail.com>
2018-02-17 20:00:13 -0500

    Initially cloned the ExData_Plotting1 repor, however does not adhere to my directory/file naming scheme, so adjusted the repo to fit mine.
    Former-commit-id: a84e4ec8c0b93e657ebba734d41870180308d8dd
    Former-commit-id: f79db391a5687075fe30b3e0f96ba71df7b59983

---
+++ projects/ExData/Project1/power_consumption_exploratory.R
@@ -0,0 +1,33 @@
+##################################################
+## Project: Exploratory Data Analysis - Week 1 Course Project
+## Script: Load power consumption data and take a look at the data (plotting)
+## Saves 4 plots
+## Source: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+## Date: 2/16/18
+## Author: Mitch Murphy
+##################################################
+
+init <- function () {
+  
+}
+
+
+
+
+
+
+load_data <- function(zipURL = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip") {
+  # Ensure that the UCI data exists in the current working directory.
+  # If it does not, download the zip file and then unzip it
+  # 
+  # Args: zipURL - string representing the URL where the zip file exists
+  # Returns: nothing, but set global variables (storing the data)
+}
+
+
+
+
+
+make_plots <- function() {
+  
+}
