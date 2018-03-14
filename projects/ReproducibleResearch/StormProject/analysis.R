fbbe026973038b7035052b3874e79ba4a5e21d8a
Mitch Murphy <mitch.murphy@gmail.com>
2018-03-14 15:04:37 -0400

    Completed basic analysis of Storm Data.
    Former-commit-id: 21dc169771a1b26e58bb1ff6d799645e9e5bacaf
    Former-commit-id: 0fdba182371838ee3b33aac027fdaa969478c9b1

--- projects/ReproducibleResearch/StormProject/analysis.R
+++ projects/ReproducibleResearch/StormProject/analysis.R
@@ -9,48 +9,67 @@ if(!file.exists("StormData.csv.bz2")) {
   download.file("https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2")
 }
 
+## Data Processing
+cache <- TRUE
+if(cache) {
+  load("stormData.RData")
+} else {
+  stormData <- as.data.table(read.csv("StormData.csv.bz2"))
+  stormData$EVTYPE <- with(stormData, as.factor(EVTYPE))
+  events <- unique(stormData$EVTYPE)
+  stormData$EVTYPENUM <- match(stormData$EVTYPE, events)
+  stormData$POPHEALTH <- stormData$FATALITIES + stormData$INJURIES
+  stormData$TOTALDMG <- stormData$PROPDMG + stormData$CROPDMG
+  stormData$DATE <- as.POSIXct(stormData$BGN_DATE, format = "%m/%d/%Y %H:%M:%S")
+  stormData$YEAR <- format(stormData$DATE, "%Y")
+  stormData$MONTH <- format(stormData$DATE, "%m")
+  # R gets stuck somewhere here...
+  #stormData$DATE <- strptime(stormData$BGN_DATE, format = "%m/%d/%Y %H:%M:%S")
+}
 
-stormData <- as.data.table(read.csv("StormData.csv.bz2"))
-str(stormData)
-
-
-# Across the United States, which types of events (as indicated in the EVTYPE variable) 
-# are most harmful with respect to population health? 
-#class(stormData$EVTYPE)
-stormData$EVTYPE <- with(stormData, as.factor(EVTYPE))
+years <- unique(stormData$YEAR)
 events <- unique(stormData$EVTYPE)
-stormData$EVTYPENUM <- match(stormData$EVTYPE, events)
-
-# health related variables: FATALITIES, INJURIES
-# scale the 2 and create one variable?
-#stormData$POP.HEALTH <- scale(stormData$FATALITIES.SCALED) + scale(stormData$INJURIES.SCALED)
-stormData$POP.HEALTH <- stormData$FATALITIES + stormData$INJURIES
 
 
-# how do we measure "population health?" 
-#   we will combine fatalaties and injuries to create a new varialbe: POP.HEALTH
-# interested in: EVTYPE, FATALITIES, INJURIES
-statsByEvent <- stormData %>% group_by(EVTYPENUM) %>% arrange(desc(POP.HEALTH)) %>% summarise(Total = sum(POP.HEALTH),
-                                                                                              Fatalities = sum(FATALITIES),
-                                                                                              Injuries = sum(INJURIES))
-statsByEvent <- cbind(statsByEvent, Event = events[statsByEvent$EVTYPENUM])
-eventsByHealth <- data.frame(Event = statsByEvent$Event, Total = statsByEvent$Total)
+## Population Health
+statsByEvent <- stormData %>% group_by(EVTYPENUM) %>% arrange(desc(POPHEALTH))
+statsByEvent <- statsByEvent %>% summarise(Total = sum(POPHEALTH), Fatalities = sum(FATALITIES), Injuries = sum(INJURIES), n = n())
+statsByEvent <- cbind(statsByEvent, Event = events[statsByEvent$EVTYPENUM], EVTYPENUM = statsByEvent$EVTYPENUM)
+statsByEvent <- statsByEvent[order(statsByEvent$Total, decreasing = T),]
 
-# sort by POP.HEALTH (desc)
-eventsByHealth <- eventsByHealth[order(eventsByHealth$Total, decreasing = T),]
-head(eventsByHealth, n = 10)
+topEventNums <- head(statsByEvent$EVTYPENUM, n=10)
+eventsYearly <- subset(stormData, EVTYPENUM %in% topEventNums) %>% group_by(EVTYPENUM, YEAR) %>% arrange(desc(POPHEALTH)) %>% summarise(TOTAL = sum(POPHEALTH), n = n())
+eventsYearly$EVENT <- events[eventsYearly$EVTYPENUM]
+eventsYearly$YEAR <- as.numeric(eventsYearly$YEAR)
+
+ggplot(eventsYearly, aes(x = YEAR, y = n, colour = EVENT)) + geom_line(aes(group = EVENT)) +
+  xlab("Year") + ylab("Frequency") + ggtitle("Frequency of storms: 1950-2018")
+
+x <- subset(eventsYearly, n >= 10000)
+x2 <- subset(eventsYearly, EVENT == "TSTM WIND")
+x3 <- subset(eventsYearly, EVENT == "THUNDERSTORM WINDS")
+
+ggplot(subset(eventsYearly, YEAR >=1988), aes(x = YEAR, y = TOTAL, colour = EVENT, lty = EVENT)) + geom_line(aes(group = EVENT)) +
+  xlab("Year") + ylab("Injuries + Fatalities") + ggtitle("Population Health vs. Year")
+
+# only focus on fatalities
+storms1 <- head(statsByEvent[order(statsByEvent$Fatalities, decreasing = T),c("Event", "Fatalities")], n = 10)$Event
+# Only focus on injuries
+storms2 <- head(statsByEvent[order(statsByEvent$Injuries, decreasing = T),c("Event", "Injuries")], n = 10)$Event
+intersect(storms1, storms2)
 
 
 
-## Across the United States, which types of events have the greatest economic consequences?
+## Economic Damages
+statsByDamage <- stormData %>% group_by(EVTYPENUM) %>% arrange(desc(TOTALDMG))
+statsByDamage <- statsByDamage %>% summarise(Total = sum(TOTALDMG), PropDmg = sum(PROPDMG), CropDmg = sum(CROPDMG))
+statsByDamage <- cbind(statsByDamage, Event = events[statsByDamage$EVTYPENUM])
+head(statsByDamage[order(statsByDamage$Total, decreasing = T),c("Event", "Total")], n = 10)
 
-# PROPDMGEXP, CROPDMGEXP
-stormData$TOTALDMG <- stormData$PROPDMG + stormData$CROPDMG
-statsByDamage <- stormData %>% group_by(EVTYPENUM) %>% arrange(desc(TOTALDMG)) %>% summarise(Total = sum(TOTALDMG),
-                                                                                              PropDmg = sum(PROPDMG),
-                                                                                              CropDmg = sum(CROPDMG))
-statsByDamage <- cbind(statsByDamage, Event = events[statsByDamage$EVTYPENUM]) #%>% select(Event, PropDmg, DropDmg)
-eventsByDamage <- data.frame(Event = statsByDamage$Event, TotalDamage = statsByDamage$Total)
-# sort by TotalDamage (decreasing)
-eventsByDamage <- eventsByDamage[order(eventsByDamage$TotalDamage, decreasing = TRUE),]
-head(eventsByDamage, n = 10)
+topEventNums.damage <- head(statsByDamage$EVTYPENUM, n=10)
+eventsYearly.damage <- subset(stormData, EVTYPENUM %in% topEventNums.damage) %>% group_by(EVTYPENUM, YEAR) %>% arrange(desc(TOTALDMG)) %>% summarise(TOTAL = sum(TOTALDMG), n = n())
+eventsYearly.damage$EVENT <- events[eventsYearly.damage$EVTYPENUM]
+eventsYearly.damage$YEAR <- as.numeric(eventsYearly.damage$YEAR)
+
+ggplot(subset(eventsYearly.damage, YEAR >= 1988), aes(x = YEAR, y = TOTAL, colour = EVENT)) + geom_line(aes(group = EVENT)) +
+  xlab("Year") + ylab("Property + Crop Damage") + ggtitle("Economic Costs From Storms, 1988-2018")
