256431403fa9ae563607b49c7eb86f2200b57d30
Mitch Murphy <mitch.murphy@gmail.com>
2018-04-17 19:20:41 -0400

    Completed statistical inference project.
    Former-commit-id: 6c5949afa86277840101a83a8609c36f6f3765c3
    Former-commit-id: e76b0204aa7add89de0460bc856179dda8cd7ba4

--- projects/StatisticalInference/project.R
+++ projects/StatisticalInference/project.R
@@ -1,5 +1,6 @@
 library(data.table)
 library(ggplot2)
+library(xtable)
 
 set.seed(round(1000*pi, 0))
 
@@ -16,3 +17,29 @@ means <- apply(simulatedExponentials, 2, mean)
 d <- density(means)
 plot(d, main = "Kernel Density of Means", xlab = "Mean")
 polygon(d, col = "red", border = "blue")
+observed_mean <- mean(means)
+legend("topleft", legend = c(paste("Observed Mean:",round(observed_mean,3)), paste("Expected Mean:",lambda^-1)),
+       lty = c(2,3), col = c("yellow", "black"), bty = "n")
+abline(v=lambda^-1, col = "yellow", lwd = 2, lty = 2)
+abline(v=observed_mean, col = "black", lty = 3, lwd = 2)
+
+# first let's compute the standard deviations
+sample_sd <- sd(means)
+expected_sd <- (1/lambda)/sqrt(n)
+# now let's focus on the variance of the 2
+sample_var <- sample_sd^2
+expected_var <- ((1/lambda)*(1/sqrt(n)))^2
+compareMatrix <- matrix(c(1/lambda,observed_mean,
+                          expected_var, sample_var), nrow = 2)
+rownames(compareMatrix) <- c("Expected","Observed")
+colnames(compareMatrix) <- c("mean", "variance")
+compareMatrix
+
+# lets overlay the normal distribution over our observed data (density/histogram)
+xfit <- seq(min(means), max(means), length=100)
+yfit <- dnorm(xfit, mean=1/lambda, sd=(1/lambda/sqrt(n)))
+hist(means,breaks=n,prob=T,col="orange",xlab = "means",main="Density of means",ylab="density")
+lines(xfit, yfit, pch=22, col="black", lty=5)
+
+qqnorm(means)
+qqline(means, col = 2)
