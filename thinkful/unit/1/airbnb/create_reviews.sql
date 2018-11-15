234d46a593e688e9b8232e80cec2e9083b6528ca
Mitch Murphy <mitch.murphy@gmail.com>
2018-11-15 14:31:36 -0500

    Cleaned up files.

--- thinkful/unit/1/airbnb/create_reviews.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE sfo_reviews (
-	listing_id BIGINT,
-	id BIGINT,
-	review_date DATE,
-	reviewer_id BIGINT,
-	reviewer_name TEXT,
-	comments TEXT
-);
--- thinkful/unit/1/airbnb/create_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE sfo_calendar (
-	listing_id BIGINT,
-	calender_date DATE,
-	available BOOLEAN,
-	price MONEY
-);
